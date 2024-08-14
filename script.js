7601ec6e4f8fddc2c0c8da67ec3519c60d3cf529
MMohamed TTalaaT <144382878+MMohamedTTalaaT@users.noreply.github.com>
2024-08-14 10:28:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// javascript
+// document.addEventListener('DOMContentLoaded', () => {
+//     // Add smooth scroll to anchor links
+//     const links = document.querySelectorAll('nav ul li a');
+
+//     for (const link of links) {
+//         link.addEventListener('click', smoothScroll);
+//     }
+
+//     function smoothScroll(e) {
+//         e.preventDefault();
+
+//         const href = this.getAttribute('href');
+//         const target = document.querySelector(href);
+
+//         if (target) {
+//             window.scroll({
+//                 top: target.offsetTop,
+//                 behavior: 'smooth'
+//             });
+//         }
+//     }
+// });
